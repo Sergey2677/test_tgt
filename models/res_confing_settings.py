f6896d5cfa311eac29b358f8f869c81fb61016c3
Sergeyasdf2677 <sergeyasdf2677@icloud.com>
2023-05-22 17:45:12 +0200

    update res config(delete unused field)

--- models/res_confing_settings.py
+++ models/res_confing_settings.py
@@ -6,5 +6,4 @@ from odoo import models, fields
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    hr_expense = fields.Many2one('hr.expense')
     accountant_user_id = fields.Many2one('res.users', related='company_id.accountant_user_id', readonly=False)
